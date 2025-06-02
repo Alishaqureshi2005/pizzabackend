87952185679ffe203c96fc689502717772ccce3c
Alishaqureshi2005 <alishaqureshi227@gmail.com>
2025-06-03 01:53:41 +0500

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../pizzahome/build')));
 app.get('*', (req, res) => {
